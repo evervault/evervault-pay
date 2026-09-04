a9a3806903f1763a8e4e4324a89c7d1d465ff54a
Max Harrison <29521329+maxharrison@users.noreply.github.com>
2026-09-04 13:23:54 +0100

    Prepare Android Google Pay SDK v1.0.0 (#101)

--- android/googlepay/src/androidTest/java/com/evervault/googlepay/ExampleInstrumentedTest.kt
+++ android/googlepay/src/androidTest/java/com/evervault/googlepay/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.evervault.googlepay.test", appContext.packageName)
+        assertEquals("com.evervault.payments.test", appContext.packageName)
     }
 }
